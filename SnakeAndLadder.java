3a56bc614361455bdb7c1cb48f21f36222110668
anuroopkanna <anuroopindia16@gmail.com>
2023-03-04 00:21:47 +0530

    added:RepeatTillReachPosition

--- SnakeAndLadder.java
+++ SnakeAndLadder.java
@@ -6,22 +6,35 @@ public class SnakeAndLadder {
     public static void main(String[] args) {
         System.out.println("Welcome to The  Snake and Ladder game");
         System.out.println("Single Player at Start Position 0");
+        int positionFirstPlayer=0;
+        boolean currentTurn=true;
+        while (positionFirstPlayer!=100){
+            if (currentTurn==true){
+                System.out.println("Player1 currently at position" +positionFirstPlayer);
         int RollDice=(int)(Math.random()*6+1);
         System.out.println("Player 1 rolls the dies and get:" +RollDice);
-        Scanner Iv=new Scanner(System.in);
-        System.out.println("Enter option: ");
-        int option=Iv.nextInt();
-        int position=0;
-        switch (option){
+        int currentOption=(int)(Math.random()*3+1);
+        switch (currentOption) {
             case 1:
-                System.out.println("NO of Play: " +position);
+                System.out.println("NO of Play: " + positionFirstPlayer);
                 System.out.println("Player stays in th same position");
+                break;
             case 2:
-                System.out.println("Ladder:" +(RollDice+position));
-                System.out.println("Player moves ahead by: " +RollDice);
+                System.out.println("Ladder:" + (RollDice + positionFirstPlayer));
+                System.out.println("Player moves ahead by: " + RollDice);
+                break;
             case 3:
-                System.out.println("Snake:" +(RollDice+position));
-                System.out.println("Player moves behind :" +RollDice);
+                System.out.println("Snake:" + (RollDice + positionFirstPlayer));
+                System.out.println("Player moves behind :" + RollDice);
+                break;
         }
+        if (positionFirstPlayer<0){
+            positionFirstPlayer=0;
+        }
+                System.out.println();
+        }
+        }
+        System.out.println("Player 1 reached the winning position 100");
     }
 }
+
