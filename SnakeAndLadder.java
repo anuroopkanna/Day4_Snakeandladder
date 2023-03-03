4aece2bca89522c59334918287990ebec000866b
anuroopkanna <anuroopindia16@gmail.com>
2023-03-03 23:48:09 +0530

    added:StatePosition

---
+++ SnakeAndLadder.java
@@ -0,0 +1,8 @@
+package com.Bridgelabz;
+
+public class SnakeAndLadder {
+    public static void main(String[] args) {
+        System.out.println("Start the snake and ladder game");
+        System.out.println("Single Player at Start Position 0");
+    }
+}
