41202b7deba41cc90c823891d0cfe4f4cd5c7629
anuroopkanna <anuroopindia16@gmail.com>
2023-03-03 23:57:10 +0530

    added:RollDice

--- SnakeAndLadder.java
+++ SnakeAndLadder.java
@@ -4,5 +4,7 @@ public class SnakeAndLadder {
     public static void main(String[] args) {
         System.out.println("Welcome to The  Snake and Ladder game");
         System.out.println("Single Player at Start Position 0");
+        int RollDice=(int)(Math.random()*6+1);
+        System.out.println("Player 1 rolls the dies and get:" +RollDice);
     }
 }
