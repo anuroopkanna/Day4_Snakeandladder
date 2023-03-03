42cc105ddb186f03636815cb87acb93a6d1f77fc
anuroopkanna <anuroopindia16@gmail.com>
2023-03-03 23:52:44 +0530

    added:StateP

--- SnakeAndLadder.java
+++ SnakeAndLadder.java
@@ -2,7 +2,7 @@ package com.Bridgelabz;
 
 public class SnakeAndLadder {
     public static void main(String[] args) {
-        System.out.println("Start the snake and ladder game");
+        System.out.println("Welcome to The  Snake and Ladder game");
         System.out.println("Single Player at Start Position 0");
     }
 }
